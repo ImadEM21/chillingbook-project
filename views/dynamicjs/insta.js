bb68269c789d7b534509e3be0ddf19afdac96c08
Imad Elmahrad <70642895+ImadEM21@users.noreply.github.com>
2020-11-19 22:36:21 +0100

    Update insta.js

--- views/dynamicjs/insta.js
+++ views/dynamicjs/insta.js
@@ -3,6 +3,6 @@ var userFeed = new Instafeed({
     target: "instafeed-container",
     resolution: 'low_resolution',
     limit: 2,
-    accessToken: 'IGQVJXVVpzc3llYVpfSUU0YUtzazdvQm1CUHpyTl83cTViRTl2NjVTMEJnYTBQT09vYjVPZAXhRRmhLUzF4VF9BSmdDeUhkcldoN0lMTlo1T0VFNy1rcTh2N2R5bmRJZAGRuSWpvZAmxnQTdaWHV3Ty1FdgZDZD'
+    accessToken: 'INSTAGRAM_TOKEN'
 });
 userFeed.run();
