06a57b5e3dd7506f4255e0023311460e169a72d5
Imad Elmahrad <70642895+ImadEM21@users.noreply.github.com>
2020-11-19 22:35:35 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
     
     var token = new Cookies(req,res).get('access_token');
 
-    jwt.verify(token, '2hU8pMq8OVN2mlJz9KwNGfksEWtIIGOJZNxm3pkoW3c88i3NhMgjE8dJHPY5DSOGDMnraXhhwUNxLuAK29WrecXKuUjqOscJcZhq', (err, decoded) => {
+    jwt.verify(token, 'RANDOM_WEB_TOKEN', (err, decoded) => {
         if (err) {
             res.status(403).json({message: "L'authentification a échoué"});
         } else {
